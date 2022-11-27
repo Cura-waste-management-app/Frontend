826aed5f8369929bf057c360f0ed8fe292616b16
rohitbajaj7877 <rohitbajaj7877@gmail.com>
2022-11-27 13:27:34 +0530

    created shared listings card

--- lib/screens/myListings/user_listings.dart
+++ lib/screens/myListings/user_listings.dart
@@ -3,9 +3,21 @@ import 'package:cura_frontend/screens/myListings/features/header.dart';
 import 'package:cura_frontend/screens/myListings/features/search_bar.dart';
 import 'package:cura_frontend/screens/myListings/features/filter.dart';
 import 'package:cura_frontend/screens/myListings/features/active_listings.dart';
+import 'package:cura_frontend/screens/myListings/features/shared_listings.dart';
 
 // ignore: use_key_in_widget_constructors
 class UserListings extends StatelessWidget {
+  final List<Widget> listings = [
+    ActiveListings(),
+    SharedListings(),
+    ActiveListings(),
+    SharedListings(),
+    ActiveListings(),
+    SharedListings(),
+    ActiveListings(),
+    SharedListings()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +33,17 @@ class UserListings extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [SearchBar(), Filter()]),
           ),
-          ActiveListings(),
-          ActiveListings(),
+          Container(
+            height: 580,
+            margin: const EdgeInsets.only(right: 3),
+            child: Scrollbar(
+              thumbVisibility: true,
+              trackVisibility: true,
+              child: ListView.builder(
+                  itemCount: listings.length,
+                  itemBuilder: (c, i) => listings[i]),
+            ),
+          ),
         ]));
   }
 }
