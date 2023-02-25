de65f38b91e421cd76986bbcb5e2ba7b601ccbb1
AmanLohan <lohanaman2000@gmail.com>
2023-02-25 19:08:01 +0530

    images_upload feature

--- lib/features/image_uploads/cloudinary_upload.dart
+++ lib/features/image_uploads/cloudinary_upload.dart
@@ -3,17 +3,16 @@ import 'package:image_picker/image_picker.dart';
 
 final cloudinary = CloudinaryPublic('dmnvphmdi', 'lvqrgqrr', cache: false);
 
-Future<String> imageUpload( img) async {
+Future<String> imageUpload(img) async {
   try {
     CloudinaryResponse response = await cloudinary.uploadFile(
       CloudinaryFile.fromFile(img.path,
           resourceType: CloudinaryResourceType.Image),
     );
-    
+
     print(response.secureUrl);
     return response.secureUrl;
-  } 
-  on CloudinaryException catch (e) {
+  } on CloudinaryException catch (e) {
     print("Ye kya hogya");
     print(e.message);
     print(e.request);
