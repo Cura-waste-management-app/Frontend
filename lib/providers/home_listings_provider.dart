1b8982465d6eeb6886711d0ecb5abb22d8dc27ba
rohitbajaj7877 <rohitbajaj7877@gmail.com>
2023-03-27 10:33:36 +0530

    changes reverted from harsh branch for home listing provider

--- lib/providers/home_listings_provider.dart
+++ lib/providers/home_listings_provider.dart
@@ -57,35 +57,108 @@ class HomeListingsNotifier extends ChangeNotifier {
 
   final baseUrl = 'http://192.168.1.6:3000';
 
-  Future<List> fetchAndSetItems() async {
+  Future<void> fetchAndSetItems() async {
     Uri url = Uri.parse(
-      "$baseUrl/homeListings/homeproducts/$uid",
+      "${base_url}/homeListings/homeproducts/${uid}",
     );
     try {
       var response = await http.get(url);
-      final data = json.decode(response.body);
-      // print(data);
-      Iterable list = data['listings'];
-      List<dynamic> itemsLiked = data['itemsLiked'] ;
-      List<dynamic> itemsRequested = data['itemsRequested'] ;
 
-     
+      final data = response.body;
+      // print(data['user']);
+      final List fetchedItems = json.decode(data)['listings'];
+      final Map userData = json.decode(data)['user'];
+      final List likedItems = userData['itemsLiked'];
+      final List reqItems = userData['itemsRequested'];
+      print(fetchedItems);
+      print(userData);
 
-      List<Listing> listings =
-          List<Listing>.from(list.map((obj) => Listing.fromJson(obj)));
+      // for (int i = 0; i < list.length; i++) {}
+      // List<Listing> listings =
+      //     List<Listing>.from(list.map((obj) => Listing.fromJson(obj)));
 
-      for (int i = 0; i < list.length; i++) {
-        listings[i].isFavourite = itemsLiked.contains(listings[i].id as dynamic);
-        listings[i].isRequested = itemsRequested.contains(listings[i].id as dynamic);
+      List<Listing> dummyList = [];
+      for (int i = 0; i < fetchedItems.length; i++) {
+        print(fetchedItems[i]['title']);
+
+        bool fav = false;
+        bool req = false;
+
+        for (int j = 0; j < reqItems.length; j++) {
+          if (fetchedItems[i]['_id'].toString() == reqItems[j].toString()) {
+            req = true;
+            break;
+          }
+        }
+
+        for (int j = 0; j < likedItems.length; j++) {
+          if (fetchedItems[i]['_id'].toString() == likedItems[j].toString()) {
+            fav = true;
+            break;
+          }
+        }
+
+        dummyList.add(Listing(
+          id: fetchedItems[i]['_id'],
+          description: fetchedItems[i]['description'],
+          title: fetchedItems[i]['title'],
+          status: fetchedItems[i]['status'],
+          requests: fetchedItems[i]['requestedUsers'].length,
+
+          likes: fetchedItems[i]['likes'],
+          isFavourite: fav,
+          isRequested: req,
+          postTimeStamp: DateTime.parse(fetchedItems[i]['postTimeStamp']),
+          location: "552 m",
+          // userImageURL: 'assets/images/female_user.png',
+          owner: "john",
+          category: fetchedItems[i]['category'],
+          imagePath: fetchedItems[i]['imagePath'],
+        ));
       }
 
-      _displayItems = listings;
+      _displayItems = dummyList;
+      for (int i = 0; i < _displayItems.length; i++) {
+        print(_displayItems[i].isFavourite);
+      }
       // print(listings);
-      notifyListeners();
-      return listings;
+      // notifyListeners();
+      // return listings;
     } catch (err) {
       throw err;
     }
     notifyListeners();
   }
+
+  // Future<List> fetchAndSetItems() async {
+  //   Uri url = Uri.parse(
+  //     "$baseUrl/homeListings/homeproducts/$uid",
+  //   );
+  //   try {
+  //     var response = await http.get(url);
+  //     final data = json.decode(response.body);
+  //     // print(data);
+  //     Iterable list = data['listings'];
+  //     List<dynamic> itemsLiked = data['itemsLiked'] ;
+  //     List<dynamic> itemsRequested = data['itemsRequested'] ;
+
+     
+
+  //     List<Listing> listings =
+  //         List<Listing>.from(list.map((obj) => Listing.fromJson(obj)));
+
+  //     for (int i = 0; i < list.length; i++) {
+  //       listings[i].isFavourite = itemsLiked.contains(listings[i].id as dynamic);
+  //       listings[i].isRequested = itemsRequested.contains(listings[i].id as dynamic);
+  //     }
+
+  //     _displayItems = listings;
+  //     // print(listings);
+  //     notifyListeners();
+  //     return listings;
+  //   } catch (err) {
+  //     throw err;
+  //   }
+  //   notifyListeners();
+  // }
 }
