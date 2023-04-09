84aaa878717ddae65e484655062329a9c3ff94d0
HarshaK47 <harsha49code@gmail.com>
2023-04-09 14:16:30 +0530

    home route

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class MyApp extends StatelessWidget {
             // is not restarted.
 
             ),
-        home: HomeListings(),
+        home: const AuthScreenPhone(),
         routes: {
           HomeListings.routeName: (ctx) => HomeListings(),
           FavouriteListingsScreen.routeName: (ctx) => FavouriteListingsScreen(),
